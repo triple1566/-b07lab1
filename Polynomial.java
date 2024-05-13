c537893f91e2e2066a9dd860ae3618fa1c5f0462
triple1566 <ljhub1566@gmail.com>
2024-05-13 15:33:40 -0400

    implement .add method

--- Polynomial.java
+++ Polynomial.java
@@ -1,5 +1,32 @@
 public class Polynomial{
-    public static void main(String[] args){
-        System.out.println("Hello World");
+    double[] arrCoefficient = {};//field to represent the polynomial via coefficients
+
+    public Polynomial(){// No-Arg constructor
+        double[] temp = {0};
+        arrCoefficient = temp;
     }
+    public Polynomial(double[] arrPol){// ArrDouble-Arg constructor with the argument that will be used to represent a non-zero Polynomial
+        arrCoefficient = arrPol;
+    }
+
+    public Polynomial add(Polynomial toAdd){//this method adds two polynomial objects
+        int lengthA = this.arrCoefficient.length;
+        int lengthB = toAdd.arrCoefficient.length;
+        int returnLength=0;
+        if(lengthA>lengthB){
+            returnLength = lengthA;
+        }
+        else{
+            returnLength = lengthB;
+        }
+        for(int i=0; i<returnLength; i++){
+            this.arrCoefficient[i] = this.arrCoefficient[i]+toAdd.arrCoefficient[i];
+        }
+        return this;
+    }
+
+    public int evaluate(double insertVal){
+
+    }
+
 }
