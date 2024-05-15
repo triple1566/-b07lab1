145932f68f910a037b5622d140384cdffc62ce2d
triple1566 <ljhub1566@gmail.com>
2024-05-15 14:59:51 -0400

    all method complete

--- Polynomial.java
+++ Polynomial.java
@@ -12,7 +12,7 @@ public class Polynomial{
     public Polynomial add(Polynomial toAdd){//this method adds two polynomial objects
         int lengthA = this.arrCoefficient.length;
         int lengthB = toAdd.arrCoefficient.length;
-        int returnLength=0;
+        int returnLength;
         if(lengthA>lengthB){
             returnLength = lengthA;
         }
@@ -26,7 +26,23 @@ public class Polynomial{
     }
 
     public int evaluate(double insertVal){
+        int returnVal=0;
 
+        int power = arrCoefficient.length-1;
+
+        for(int i=0; i<=power; i++){
+            returnVal+=arrCoefficient[i]*Math.pow(insertVal, i);
+        }
+
+        return returnVal;
+    }
+
+    public boolean hasRoot(double potRoot){
+
+        if(evaluate(potRoot)==0){
+            return true;
+        }
+        return false;
     }
 
 }
