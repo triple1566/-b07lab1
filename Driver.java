6ed1b3d160b173cde3c7bdb636035e65517032d1
triple1566 <ljhub1566@gmail.com>
2024-05-16 09:03:29 -0400

    custom test

--- Driver.java
+++ Driver.java
@@ -1,3 +1,5 @@
+
+
 public class Driver { 
     public static void main(String [] args) { 
         Polynomial p = new Polynomial(); 
@@ -10,5 +12,18 @@ public class Driver {
         System.out.println("s(0.1) = " + s.evaluate(0.1)); 
         if(s.hasRoot(1)) System.out.println("1 is a root of s"); 
         else System.out.println("1 is not a root of s"); 
+
+        //================custom test
+        double[] a3 = {1,2,1};
+        Polynomial p3 = new Polynomial(a3);
+        System.out.println("p3 at 1 is "+p3.evaluate(1));
+        System.out.println("3 is p3's root? "+p3.hasRoot(0));
+        System.out.println("-1 is p3's root? "+p3.hasRoot(-1));
+
+        double[] a4 = {1};
+        Polynomial p4 = new Polynomial(a4);
+        System.out.println(p4.evaluate(1));        
+        System.out.println(p4.evaluate(10000));
+        System.out.println(p4.hasRoot(0));        
     } 
 }
